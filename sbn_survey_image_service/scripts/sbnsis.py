850d75c3d02f410abcf69ee0efb11afa8b375dcc
Michael S. P. Kelley <msk@astro.umd.edu>
2023-02-25 11:27:48 -0500

    code formatting

--- sbn_survey_image_service/scripts/sbnsis.py
+++ sbn_survey_image_service/scripts/sbnsis.py
@@ -1,10 +1,25 @@
+#!/usr/bin/env python3
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import os
-from typing import List, Union
+from typing import List
 import argparse
 from argparse import ArgumentParser
 import subprocess
+
+
+class colors:
+    """Terminal color codes."""
+
+    reset = "\033[00m"
+    black = "\033[30m"
+    red = "\033[31m"
+    green = "\033[32m"
+    yellow = "\033[33m"
+    blue = "\033[34m"
+    magenta = "\033[35m"
+    cyan = "\033[36m"
+    white = "\033[37m"
 
 
 def start_service(args: argparse.Namespace) -> None:
@@ -28,15 +43,17 @@ def status_service(args: argparse.Namespace) -> int:
 
     check_venv()
 
-    processes: List[str] = subprocess.check_output(
-        ["ps", "-ef"]).decode().splitlines()
+    processes: List[str] = subprocess.check_output(["ps", "-ef"]).decode().splitlines()
     venv: str = os.getenv("VIRTUAL_ENV")
-    gunicorn_processes: List[str] = [process for process in processes
-                                     if f"{venv}/bin/sbnsis-service" in process]
+    gunicorn_processes: List[str] = [
+        process for process in processes if f"{venv}/bin/sbnsis-service" in process
+    ]
 
     n: int = len(gunicorn_processes)
     if n == 0:
-        print("No sbnsis-service is running from this project's virtual environment at the moment.")
+        print(
+            "No sbnsis-service is running from this project's virtual environment at the moment."
+        )
         return 0
 
     ppid: str
@@ -45,8 +62,10 @@ def status_service(args: argparse.Namespace) -> int:
         if ppid != "1":
             break
 
-    print(f"""sbnsis-service is running with {n} processes.
-Parent PID: {ppid}""")
+    print(
+        f"""sbnsis-service is running with {n} processes.
+Parent PID: {ppid}"""
+    )
 
     return n
 
@@ -56,34 +75,40 @@ def stop_service(args: argparse.Namespace) -> None:
 
 
 def parse_arguments() -> ArgumentParser:
-    parser: ArgumentParser = ArgumentParser(
-        description="SBN Survey Image Service")
+    parser: ArgumentParser = ArgumentParser(description="SBN Survey Image Service")
     subparsers: argparse._SubParsersAction = parser.add_subparsers(
-        help="sub-command help")
+        help="sub-command help"
+    )
 
     # start #########
     start_parser: ArgumentParser = subparsers.add_parser(
-        "start", help="start the service")
+        "start", help="start the service"
+    )
     start_parser.set_defaults(func=start_service)
     start_parser.add_argument(
-        "--dev", action="store_true", help="run in development mode")
+        "--dev", action="store_true", help="run in development mode"
+    )
     start_parser.add_argument(
-        "--no-daemon", dest="daemon", action="store_false",
-        help="do not run in daemon mode (implied with --dev)")
+        "--no-daemon",
+        dest="daemon",
+        action="store_false",
+        help="do not run in daemon mode (implied with --dev)",
+    )
 
     # restart #######
     restart_parser: ArgumentParser = subparsers.add_parser(
-        "restart", help="restart the service")
+        "restart", help="restart the service"
+    )
     restart_parser.set_defaults(func=restart_service)
 
     # status ########
     status_parser: ArgumentParser = subparsers.add_parser(
-        "status", help="get service status")
+        "status", help="get service status"
+    )
     status_parser.set_defaults(func=status_service)
 
     # stop ##########
-    stop_parser: ArgumentParser = subparsers.add_parser(
-        "stop", help="stop the service")
+    stop_parser: ArgumentParser = subparsers.add_parser("stop", help="stop the service")
     stop_parser.set_defaults(func=stop_service)
 
     return parser
@@ -93,7 +118,7 @@ if __name__ == "__main__":
     parser: ArgumentParser = parse_arguments()
     args: argparse.Namespace = parser.parse_args()
 
-    if hasattr(args, 'func'):
+    if hasattr(args, "func"):
         args.func(args)
     else:
         parser.print_usage()
